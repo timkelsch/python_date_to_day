a5d3556e2f174058f52a591ae1506ac9c9fcee17
Tim Kelsch <timkelsch@gmail.com>
2024-08-26 23:22:56 -0600

    addressed PR comments

--- date_to_day.py
+++ date_to_day.py
@@ -2,17 +2,19 @@ from datetime import datetime
 import logging
 import os
 
-def gather_input(max_attempts=3):
-    for _ in range(max_attempts):
+MAX_ATTEMPTS = 3
+
+def gather_input(max_attempts):
+    for attempt in range(max_attempts):
         date = input("Enter a date (mm/dd/yyyy): ")
-        logger.info(f"User input: {date}")
+        logger.debug(f"User input: {date}")
         try:
             parsed = datetime.strptime(date, "%m/%d/%Y")
-            logger.info(f"Parsed date: {parsed}")
+            logger.debug(f"Parsed date: {parsed}")
             return parsed
         except ValueError:
             logger.error("Error - Date Format:", exc_info=True)
-            print("Invalid date format. Please try again.")
+            print("Invalid date format. You have " + str(MAX_ATTEMPTS - attempt - 1) + " attempts remaining. Please try again.")
 
     message = "Max attempts reached. Please try again."
     logger.critical(f"{message}")
@@ -20,8 +22,7 @@ def gather_input(max_attempts=3):
 
 def calculate_day_of_week(date):
     day_of_week = date.strftime("%A")
-    logger.info(f"Type of day_of_week: {type(day_of_week)}")
-    logger.info(f"Value of day_of_week: {day_of_week}")
+    logger.debug(f"Value of day_of_week: {day_of_week}")
     return day_of_week
 
 def setup_logging():
@@ -41,8 +42,12 @@ def setup_logging():
 logger = setup_logging()
 
 if __name__ == '__main__':
-    logger.info('Application started')
+    try:
+        logger.info('Application started')
+        date = gather_input(MAX_ATTEMPTS)
+        print(calculate_day_of_week(date))
+        logger.info('Application finished successfully.')
     
-    date = gather_input()
-    print(calculate_day_of_week(date))
-    logger.info('Application finished')
+    except ValueError as e:
+        logger.critical(f'Application exiting due to error: {str(e)}', exc_info=True)
+        print(f"Error: {str(e)}")
